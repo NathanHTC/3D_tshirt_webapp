2e020b9e26ef1613bc4f1b118ddce9689a424d74
CharlesvSolomon <000savior001@gmail.com>
2023-11-21 22:31:02 -0800

    added rendering animation in cameraRig.jsx

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -9,21 +9,23 @@ const CameraRig = ({ children }) => {
   const group = useRef();
   const snap = useSnapshot(state);
 
-  //set the camera position smoothly
-  useFrame()
-
-  //set the model rotation smoothly
-  easing.dampE(
-    group.current.rotation, 
-    [state.pointer.y / 10, -state.pointer.x / 5, 0],
-    0.25,
-  )
-
+  
+  useFrame((state, delta) => {
+    
+    const isBreakpoint = window.innerWidth <= 1260;
+    // set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation, 
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
   return (
     <group ref={group}>
         {children}
     </group>
   )
-}
+ }
 
 export default CameraRig
