c4a422091e3192ccc6a8e8485456eaeb7f39fe76
CharlesvSolomon <000savior001@gmail.com>
2023-11-30 13:07:31 -0800

    cameraRig: all damping, transition done

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -20,20 +20,20 @@ const CameraRig = ({ children }) => {
     //set the initial position of the model
     let targetPosition = [-0.4, 0, 2];
     if(snap.intro){
-      if(isBreakpoint) targetPosition = [0, 0, 2];
+      if(isBreakpoint) targetPosition = [-0.2, 0, 2];
       if(isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
       if(isMobile) targetPosition = [0, 0, 2.5];
       else targetPosition = [0, 0, 2];
     }
 
-    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+    easing.damp3(state.camera.position, targetPosition, 0.2, delta);
     
 
     // set the model rotation smoothly
     easing.dampE(
       group.current.rotation, 
-      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      [state.pointer.y / 5, -state.pointer.x / 3, 0],
       0.25,
       delta
     )
