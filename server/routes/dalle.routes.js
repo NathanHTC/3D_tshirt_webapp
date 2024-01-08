7034210c99a6feae4ea9a6d39afd37f15217195a
CharlesvSolomon <000savior001@gmail.com>
2024-01-07 17:16:49 -0800

    dalleRouter all set

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import {  OpenAI } from 'openai';
 
-
+//so we could access secretive .env file 
 dotenv.config();
 
 const openai = new OpenAI({
@@ -14,18 +14,22 @@ router.route('/').get((req, res) => {
     res.status(200).json({ message: "hello from DALL.E ROUTES" })
 })
 
-//we need a new route to pass prompt from frontend to server
+//we need a new route to pass prompt from frontend to openai
+//and then send back the generated image as json obj
 router.route('/').post(async (req, res) => {
     try{
         const { prompt } = req.body;
 
-        const response = await openai.images.generate({
+        const response = await openai.createImage({
+            
             prompt,
             n:1,
-            size: '1024x1024',
+            size: "1024x1024",
             response_format: 'b64_json'
         });
+        
         const image = response.data.data[0].b64_json;
+        //sent back to the front end as json object
         res.status(200).json({ photo: image });
 
     } catch(error){
