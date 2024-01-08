45cb0f20d5cd2cfff5be91cfa216f847eb03e44e
CharlesvSolomon <000savior001@gmail.com>
2024-01-07 19:48:29 -0800

    trying to problem shoot CORS problem

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,44 +1,39 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import {  OpenAI } from 'openai';
+import { Configuration, OpenAIApi} from 'openai';
 
-//so we could access secretive .env file 
 dotenv.config();
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-})
 const router = express.Router();
 
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
 router.route('/').get((req, res) => {
-    res.status(200).json({ message: "hello from DALL.E ROUTES" })
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
 })
 
-//we need a new route to pass prompt from frontend to openai
-//and then send back the generated image as json obj
 router.route('/').post(async (req, res) => {
-    try{
-        const { prompt } = req.body;
+  try {
+    const { prompt } = req.body;
 
-        const response = await openai.images.generate({
-            model: "dall-e-3",
-            prompt: prompt,
-            n:1,
-            size: "1024x1024"
-        });
-        
-        
-        const image = response.data[0].url;
-        console.log(image);
-        //image contains data url
-        res.status(200).json({ photo: image });
-       
-        
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json'
+    });
 
-    } catch(error){
-        console.error(error);
-        res.status(500).json({ message: "Something went wrong" })
-    }
+    const image = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" })
+  }
 })
 
 export default router;
